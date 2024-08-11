46c78d0187b146dd017210080652a6f380dbe0a8
sahatree <potaesht@gmail.com>
2024-08-11 15:44:26 +0700

    First Upload

---
+++ python1.py
@@ -0,0 +1,90 @@
+"""
+#
+# Part: Python comment
+#
+"""
+
+# This isa comment
+# v = s/t
+# v = ความเร็ว(m/s)
+# s = ระยะทาง(m)
+# t = เวลา (s)
+
+"""
+# v = s/t
+# v = ความเร็ว(m/s)
+# s = ระยะทาง(m)
+# t = เวลา(s)
+"""
+
+print("Hello World")
+
+"""
+#
+# Part: Python Variables
+"""
+x = s #Integer
+y = "Hey Brus"#String
+print(x,y)
+
+x = Str(3)
+y = int(5)
+z = float(7)
+print(type(x), type(y), type(z))
+
+
+
+# Part: Variables Names
+#
+"""
+myvar= "John"
+my_var= "John"
+_my_var= "John"
+myVar= "John"
+MYVAR= "John"
+MY_VAR= "John"
+my_var2= "John"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+2 Hey Brus
+3 Hey Brus
+"""
+print(y)
+
+x = "Hey Brus"
+print(x[2])
+print(len(x))
+print("Hey" in x)
+print("What'sup" not in x)
+print(x.upper())
+print(x.lower())
+
+print(x.replace("Brus", "Sis"))
+print(x.split(""))
+
+a="Apple"
+b="Company"
+print(a+""+b)
+
+
+print=20
+word=f"Price:{Price:.2f}"
+print(word)
---
+++ python2.py
@@ -0,0 +1,14 @@
+"""
+#
+# Part: Python Operators
+# Arithmetic Operators
+#
+"""
+x = 4 + 5
+print ("x = 4 + 5--->", x
+)
+
+x = 5
+y = 6
+z = x + y + 1
+print("z = x + y + 1--->", z)
